268993b52ac3392eebcb2519ea616e5ba2813c56
David G. Moore, Jr. <dev@dgmjr.io>
2023-12-23 22:36:01 -0500

    fix node stuff

--- js/catalogschemas.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = __importDefault(require("fs"));
-var schemas = fs_1.default.readdirSync("..").filter(file => file.endsWith(".schema"));
-fs_1.default.writeFileSync("schemas.json", JSON.stringify(schemas));
