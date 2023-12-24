92e0dad05c601538b2e85aa9c0cd27dd4561f576
David G. Moore, Jr. <dev@dgmjr.io>
2023-12-23 23:44:22 -0500

    fix the import problem with commas

--- src/ts/makeschemasdoc.ts
+++ src/ts/makeschemasdoc.ts
@@ -1,11 +1,12 @@
 import * as schemas from "./schemas.json";
 import jquery from "jquery";
 
+const _schemas = schemas.filter(s => !/,/.test(s));
+
 jquery(document).ready(() => {
-    alert(`Schemas: ${JSON.stringify(schemas)}`);
     const schemasPlaceholder = jquery("#schemas");
     if (schemasPlaceholder) {
-        for (let schema in schemas) {
+        for (let schema in _schemas) {
             const schemaLi = jquery(document.createElement("li"));
             const schemaLink = jquery(document.createElement("a"));
             schemaLink.attr("href", "schemas/" + schemas[schema]);
