49b26eab9c18732e6e69937ac8c2fd6d6893cc89
David G. Moore, Jr. <dev@dgmjr.io>
2023-12-24 00:04:06 -0500

    mebbe't's fixed now?

--- src/ts/makeschemasdoc.ts
+++ src/ts/makeschemasdoc.ts
@@ -1,19 +1,19 @@
 import * as schemas from "./schemas.json";
 import jquery from "jquery";
 
-const _schemas = new Array<string>();
-schemas.forEach(s => { if(s.indexOf(',') < 0) _schemas.push(s); })
 
 jquery(document).ready(() => {
     const schemasPlaceholder = jquery("#schemas");
     if (schemasPlaceholder) {
-        for (let schema in _schemas) {
-            const schemaLi = jquery(document.createElement("li"));
-            const schemaLink = jquery(document.createElement("a"));
-            schemaLink.attr("href", "schemas/" + schemas[schema]);
-            schemaLink.text(schemas[schema]);
-            schemaLi.append(schemaLink);
-            schemasPlaceholder.append(schemaLi);
+        for (let schema in schemas) {
+            if (schema.indexOf(',') < 0) {
+                const schemaLi = jquery(document.createElement("li"));
+                const schemaLink = jquery(document.createElement("a"));
+                schemaLink.attr("href", "schemas/" + schemas[schema]);
+                schemaLink.text(schemas[schema]);
+                schemaLi.append(schemaLink);
+                schemasPlaceholder.append(schemaLi);
+            }
         }
     }
 });
