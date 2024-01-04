68cd9ee9ded6c7bfa0160b14d0af50fd71639853
David G. Moore, Jr. <dev@dgmjr.io>
2024-01-04 17:16:01 -0500

    added swagger doc schemas

--- src/ts/makeschemasdoc.ts
+++ src/ts/makeschemasdoc.ts
@@ -1,19 +1,25 @@
-import * as schemas from "./schemas.json";
 import jquery from "jquery";
+import fetch from "node-fetch";
 
+const url = new URL(window.location.href);
+const schemasUrl = `${url.protocol}://${url.host}:${url.port}/schemas/list-all`;
+
+const fetchSchemas: () => Promise<{ [key: string]: string }> = (async () => (await fetch(schemasUrl)).json());
 
 jquery(document).ready(() => {
     const schemasPlaceholder = jquery("#schemas");
     if (schemasPlaceholder) {
-        for (let schema in schemas) {
-            if (schema.indexOf(',') < 0) {
-                const schemaLi = jquery(document.createElement("li"));
-                const schemaLink = jquery(document.createElement("a"));
-                schemaLink.attr("href", "schemas/" + schemas[schema]);
-                schemaLink.text(schemas[schema]);
-                schemaLi.append(schemaLink);
-                schemasPlaceholder.append(schemaLi);
+        (async () => await fetchSchemas())().then((schemas) => {
+            for (let schema in schemas) {
+                if (schema.indexOf(',') < 0) {
+                    const schemaLi = jquery(document.createElement("li"));
+                    const schemaLink = jquery(document.createElement("a"));
+                    schemaLink.attr("href", "schemas/" + schemas[schema]);
+                    schemaLink.text(schemas[schema]);
+                    schemaLi.append(schemaLink);
+                    schemasPlaceholder.append(schemaLi);
+                }
             }
-        }
+        })
     }
 });
